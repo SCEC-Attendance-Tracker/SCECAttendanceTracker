dfb5902cdc7f12a5dff7b03478214417bbf04584
root <root@localhost>
2021-10-03 20:29:34 +0000

    member CRUD

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,57 @@
+class MembersController < ApplicationController
+  def new
+    @member = Member.new
+  end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      # probably not gonna be this route
+      redirect_to(members_path)
+    else
+      render('new')
+    end
+  end
+
+  def index
+    @member = Member.order('id ASC')
+  end
+
+  def show
+  end
+
+  def delete
+    @member = Member.find(params[:id])
+  end
+
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    respond_to do |format|
+      format.html {redirect_to members_url, notice: 'Account deleted!'}
+      format.json {head :no_content}
+    end
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to(member_path(@member))
+    else
+      render('edit')
+    end
+  end
+
+  private
+  def member_params
+    params.require(:member).permit(:name, :email)
+  end
+
+  def set_member
+    @member = Member.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :members do
+    member do
+      get :delete
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'members/index', to: 'members#index'
+      post 'members/create', to: 'members#create'
+      delete 'members/:id', to: 'members#destroy'
+    end
+  end
+
+  root 'members#index'
 end

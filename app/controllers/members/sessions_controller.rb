8f104d56c192d46eaa35758b7b8db0c0cbce7bfe
kang-n <kyanan08@gmail.com>
2021-10-03 05:09:27 +0000

    changed username in git

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Members::SessionsController < Devise::SessionsController
+class Members::SessionsController < Devise::SessionsController 
     def after_sign_out_path_for(_resource_or_scope)
         new_member_session_path
     end

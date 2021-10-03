ab5b2acb6b7b81b31d3eb6239288c8a22fe5cbbb
Felicia I <finvedy@gmail.com>
2021-10-02 23:58:38 -0500

    Attendance view working
    Attendance view working with dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,71 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+member = Member.create([
+    { 
+        admin: 1,
+        first_name: "Steve",
+        last_name: "Jobs",
+        email: "steve.jobs@mail.com",
+        description: "apple woo",
+        paid_dues: 1,
+        total_attendance: 2
+    }, 
+    
+    { 
+        admin: 0,
+        first_name: "Frodo",
+        last_name: "Baggins",
+        email: "frodo.baggins@hobbit.com",
+        description: "me short",
+        paid_dues: 0,
+        total_attendance: 1
+    }]
+)
+
+event = Event.create([
+    { 
+        title: "WWDC 1976",
+        start_date: Time.new(1976,01,06, 11, 25, 00, "+00:00"),
+        end_date: Time.new(1976,01,06, 12, 25, 00, "+00:00"),
+        description: "apple woo",
+        avg_rating: 4.4,
+        location: "Neverland"
+    }, 
+    
+    { 
+        title: "Hundred Acre Woods Carrot Festival",
+        start_date: Time.new(2000,06,24, 12, 00, 00, "+00:00"),
+        end_date: Time.new(2000,06,24, 03, 30, 00, "+00:00"),
+        description: "eat hunny",
+        avg_rating: 4.8,
+        location: "Hundred Acre Woods"
+    },
+
+    { 
+        title: "Tea Party",
+        start_date: Time.new(2045,10,06, 03, 00, 00, "+00:00"),
+        end_date: Time.new(2045,10,06, 05, 00, 00, "+00:00"),
+        description: "have fun with rabbit and cat",
+        avg_rating: 2.5,
+        location: "Boston"
+    }]
+)
+
+attendance = Attendances.create([
+    { 
+        member_id: 1,
+        event_id: 1
+    }, 
+    
+    { 
+        member_id: 1,
+        event_id: 2
+    },
+
+    { 
+        member_id: 2,
+        event_id: 3
+    }]
+)

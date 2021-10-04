b95495c60f2d54eda804345d9d3753eeb8bc33d0
Felicia I <finvedy@gmail.com>
2021-10-03 19:39:54 -0500

    SAT-3 integrating members with attendance

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,20 @@ Rails.application.routes.draw do
     get 'members/sign_in', to: 'members/sessions#new', as: :new_member_session
     get 'members/sign_out', to: 'members/sessions#destroy', as: :destroy_member_session
   end
+
+  resources :members do
+    member do
+      get :delete
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'members/index', to: 'members#index'
+      post 'members/create', to: 'members#create'
+      delete 'members/:id', to: 'members#destroy'
+    end
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

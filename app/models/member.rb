c58145c2e9bc8b74da986fa817e69f6fc6593acb
kang-n <kyanan08@gmail.com>
2021-10-04 23:58:34 -0500

    member change

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_google(email:, first_name:, last_name:)
-      create_with(email: email, first_name: first_name, last_name: last_name).find_or_create_by!(email: email)
+      create_with(email: email, first_name: first_name, last_name: last_name, total_attendance: 0).find_or_create_by!(email: email)
   end
 
   validates :first_name, presence: true

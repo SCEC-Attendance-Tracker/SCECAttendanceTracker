b32d1fe54445b8f455918a7736c6e58c9ca5cec0
root <root@localhost>
2021-10-05 01:49:13 +0000

    added personal profile page functionality

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -1,2 +1,13 @@
 module MemberHelper
+  def is_edit_owner(id, session_id)
+    if id == session_id
+      return link_to('Edit My Page', edit_member_path(id))
+    end
+  end
+
+  def is_delete_owner(id, session_id)
+    if id == session_id
+      return link_to('Delete My Profile', delete_member_path(id))
+    end
+  end
 end

76d4b59618a079f3192659da311f45e71ae81ba2
root <root@localhost>
2021-10-22 11:01:52 +0000

    SAT-28 Added Basic Functionality for Feedback Admin View

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Feedback < ApplicationRecord
-  belongs_to :event
+  validates :event_id, presence: true
+  validates :event_review, presence: true
+  validates :event_rating_score, presence: true
 end

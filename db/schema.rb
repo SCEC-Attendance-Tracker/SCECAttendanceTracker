24dc922049678b5abadb015da653f58d7e2fb8d8
wopara1 <14403+wopara1@users.noreply.github.tamu.edu>
2021-11-01 14:48:08 -0500

    change to feedbacks/index

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,6 @@ ActiveRecord::Schema.define(version: 2021_10_03_040234) do
   create_table "attendances", force: :cascade do |t|
     t.bigint "member_id"
     t.bigint "event_id"
-    t.boolean "rsvp"
-    t.boolean "attended"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

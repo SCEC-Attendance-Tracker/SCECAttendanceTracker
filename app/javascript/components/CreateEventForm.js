e9db06657f0f85dcd4417ba8816f0e7c12aa31f7
noahjminer <noahjminer@gmail.com>
2021-11-02 19:44:29 -0500

    component made

---
+++ app/javascript/components/CreateEventForm.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class CreateEventForm extends React.Component {
+    constructor(props) {
+        super(props); 
+        this.state = { // gonna need event JSON
+            event = {}
+        };
+    }
+
+    componentDidMount = () => {
+
+    }
+
+    render = () => {
+
+    }
+}
+
+export default CreateEventForm; 

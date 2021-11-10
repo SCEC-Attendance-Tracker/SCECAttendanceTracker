1a2400ddd21bc5829fbe6dfaf677b079baf74f7a
root <root@localhost>
2021-11-10 11:59:19 +0000

    SAT-89 Created Page w/ Admin/Member Specific Components

--- app/javascript/components/LinkDataTable.js
+++ app/javascript/components/LinkDataTable.js
@@ -1,72 +1,119 @@
-import LinkTable from "./LinkTable";
-import Link from "@material-ui/core/Link";
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { Button, Link } from "@material-ui/core";
 
-function getData(props) {
-  
-  var links = props.props.links;
-  
-  const columns = [
-    {
-      headerClassName: 'theme-header',
-      field: 'id',
-      headerName: 'ID',
-      hide: true
-    },
-    { 
-      headerClassName: 'theme-header',
-      field: 'name', 
-      headerName: 'Name',
-      minWidth: 150,
-      flex: 1
-    },
-    { 
-      headerClassName: 'theme-header',
-      field: 'url', 
-      headerName: 'URL',
-      minWidth: 150,
-      flex: 1,
-    },
-    { 
-      headerClassName: 'theme-header',
-      field: 'description', 
-      headerName: 'Description',
-      minWidth: 150,
-      flex: 1
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {}
+  })
+);
+
+const handleClick = (event, cellValues) => {
+  console.log(cellValues.row);
+};
+
+const handleCellClick = (param, event) => {
+  event.stopPropagation();
+};
+
+const columns = [
+  {
+    headerClassName: 'theme-header', 
+    field: 'id', 
+    headerName: 'ID',
+    hide: true
+  },
+  { 
+    headerClassName: 'theme-header',
+    field: 'name', 
+    headerName: 'Name',
+    hide: true
+  },
+  {
+    headerClassName: 'theme-header',
+    field: 'url',
+    headerName: 'URL',
+    minWidth: 150,
+    flex: 1,
+    renderCell: (cellValues) => {
+      return <Link href={cellValues.row.url}>{cellValues.row.name}</Link>;
     }
-  ];
-  
-  var test_rows = [
-    {
-      id: 1,
-      name: 'Google',
-      url: 'https://www.google.com/',
-      description: 'Google Homepage'
-    }
-  ];
-  
-  var rows = [];
-  
-  for (var i in links) {
-    console.log(links[i])
-    
-    var entry = {
-      id: i,
-      name: links[i].name,
-      url: links[i].url,
-      description: links[i].description
-    }
-    rows.push(entry)
+  },
+  { 
+    headerClassName: 'theme-header',
+    field: 'description', 
+    headerName: 'Description',
+    minWidth: 150,
+    flex: 1
   }
-  
-  var data = {columns: columns, rows: rows}
-  return data;
-}
+];
 
-var data;
 
 export default function LinkDataTable(props) {
-  data = getData(props);
+  var member;	
+  if (props.props.member != undefined) {
+    member = props.props.member[0]
+  }
+  var rows = props.props.links;
+  const classes = useStyles();
+  { (member != undefined) && (member.admin) &&
+    columns.push(
+      { 
+        headerClassName: 'theme-header',
+        field: 'edit', 
+        headerName: 'Edit',
+        minWidth: 50,
+        maxWidth: 100,
+        flex: 1, 
+        renderCell: (cellValues) => {
+          return (
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={(event) => {
+                handleClick(event, cellValues);
+              }}
+            >
+              Edit
+            </Button>
+          );
+        }
+      },
+      { 
+        headerClassName: 'theme-header',
+        field: 'delete', 
+        headerName: 'Delete',
+        minWidth: 50,
+        maxWidth: 100,
+        flex: 1, 
+        renderCell: (cellValues) => {
+          return (
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={(event) => {
+                handleClick(event, cellValues);
+              }}
+            >
+              Delete
+            </Button>
+          );
+        }
+      }
+    )
+  }
+
   return (
-    LinkTable(data)
+    <div style={{ height: 500, width: "100%" }}>
+      <DataGrid
+        rowHeight={120}
+        className={classes.root}
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        onCellClick={handleCellClick}
+      />
+    </div>
   );
 }
